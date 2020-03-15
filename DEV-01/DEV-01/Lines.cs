7e105a951d59f41f6942f885b0bd8f9537c22611
Alexey Myalick <myalick2017@yandex.ru>
2020-03-16 00:05:02 +0300

    Add class and tests

---
+++ DEV-01/DEV-01/Lines.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace DEV_1
+{
+    public class Lines 
+    {
+        private string line;
+
+        /// <summary>
+        /// constructor initializes class elements
+        /// </summary>
+        public Lines(string line)
+        {
+            Line = line;
+        }
+
+        /// <summary>
+        /// method Line accepts and returns values
+        /// </summary>
+        public string Line
+        {
+            get
+            {
+                return line;
+            }
+
+            set
+            {
+                CheckValue(value);
+                line = value.ToLower();
+            }
+        }
+
+        /// <summary>
+        /// method LineLength returns line length
+        /// </summary>
+        public int LineLength
+        {
+            get
+            {
+                return Line.Length;
+            }
+
+        }
+
+        /// <summary>
+        /// method ComparisonOfLetter compares letters and counts the number of non-repeating
+        /// </summary>
+        public int ComparisonOfLetter(int count, int i)
+        {
+            for (int j = i + 1; j < LineLength; j++)
+            {
+                if (Equals(Line[i], Line[j]) == false)
+                {
+                    count++;
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// methode Country counts the final amount
+        /// </summary>
+        public int Counting()
+        {
+            int count = 0;
+
+            for(int i = 0; i < LineLength; i++)
+            {
+                int finalAmount = 1;
+                finalAmount = ComparisonOfLetter(finalAmount, i);
+                count = Math.Max(finalAmount, count);
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// the method CheckValue checks the value and throws an error if it is Null
+        /// </summary>
+        void CheckValue(object value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+        }
+
+    }
+}
---
+++ DEV-01/DEV-01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DEV_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lines lines = new Lines("jjjsnn\ndsds");
+            Console.WriteLine(lines.Line);
+            Console.WriteLine(lines.LineLength);
+            Console.WriteLine(lines.Counting());
+        }
+    }
+}
